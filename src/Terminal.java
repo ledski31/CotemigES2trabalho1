d47b59f1b57cdd149f0507be7e04ca6f36d264f8
Leandro <linoboy@gmail.com>
2017-10-25 20:43:03 -0200

    Estrutura inicial das classes

---
+++ src/Terminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Terminal {
+	public Terminal() {
+		
+	}
+
+	public void Menu() {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+	}
+
+	public void Saque() {
+
+	}
+
+	public void Deposito() {
+
+	}
+
+	public void Saldo() {
+
+	}
+
+	public void Extrato() {
+		
+	}
+}
