4bf2b691993c08346675669aaea36b658df62c33
Ulisses <ulisses@cotemig.com>
2017-10-27 20:42:50 -0200

    Subclasse Movimentacao

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,28 +1,51 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Terminal {
-	public Terminal() {
-		
+	private int id;
+	private double total;
+	private List<Mov> extrato;
+	
+	public Terminal(int id) {
+		this.id = id;
+		extrato = new ArrayList<Mov>();
 	}
 
 	public void Menu() {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner( System.in );
 		int i = sc.nextInt();
 	}
 
-	public void Saque() {
-
+	public void Saque( double valor ) {
+		extrato.add( new Mov( valor ) );
 	}
 
-	public void Deposito() {
-
+	public boolean Deposito( double valor ) {
+		extrato.add( new Mov( valor ) );
+		return true;
 	}
 
-	public void Saldo() {
-
+	public double Saldo() {
+		return total;
 	}
 
 	public void Extrato() {
-		
+
 	}
+
+	class Mov {
+		public LocalDateTime data;
+		public double valor;
+		public double saldo; 
+
+		public Mov(double valor) {
+			total += valor;
+			this.saldo = total;
+			this.data = LocalDateTime.now();
+			this.valor = valor;
+		}
+	}
+
 }
